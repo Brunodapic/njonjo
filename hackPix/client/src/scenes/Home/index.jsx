6115cb966f927f3e7cfdec069572fa61698f2f33
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2023-04-01 13:18:26 +0200

    home

--- hackPix/client/src/scenes/Home/index.jsx
+++ hackPix/client/src/scenes/Home/index.jsx
@@ -1,17 +1,28 @@
-import { memo, useState } from "react";
-
+import { memo, useState, useEffect } from "react";
+import axios from "axios";
 import Robot from "../../components/Robot";
+import ScoreboardItem from "../../components/ScoreboardItem/ScoreboardItem";
 
 const Home = () => {
-    const [scoreBoard, setScoreboard] = useState('');
+  const [scoreBoard, setScoreboard] = useState([]);
+  console.log(scoreBoard);
+  const getScoreboard = async () => {
+    const req = await axios.get("api/user/");
+    console.log(req.data);
+    const data=req.data.sort((a,b) => b.score - a.score); // b - a for reverse sort
 
-    
+    setScoreboard(data);
+  };
 
-    return (
-        <div>
-            {robots.map((robot) => <Robot name={`${robot._id}: ${robot.name}`} type={robot.type} key={robot._id} />)}
-        </div>
-    );
-}
+  useEffect(() => {
+    getScoreboard();
+  }, []);
+
+  return <div>
+   {scoreBoard.map((scoreElement)=>{
+    return <ScoreboardItem key={scoreElement.username} username={scoreElement.username} score={scoreElement.score}/>
+   })}
+  </div>;
+};
 
 export default memo(Home);
